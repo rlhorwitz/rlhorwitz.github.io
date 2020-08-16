188a3ecf834d8c79993a97281d5e66099094852e
Rachel Horwitz <rachel@Rachels-MacBook-Pro.local>
2020-08-15 23:55:32 -0400

    Updates

---
+++ precache-manifest.2f2bad2cc3a37f7ccc79e4c225eaf5e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547d654ad78a62c1484f8da042e870d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aee8c8e3dd9ca7848a0b",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "aee8c8e3dd9ca7848a0b",
+    "url": "/static/js/2.f6390a03.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f6390a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f92a1302be95bfb4ba",
+    "url": "/static/js/main.f1ed0c9f.chunk.js"
+  },
+  {
+    "revision": "b69dc2d0859210660ca6",
+    "url": "/static/js/runtime-main.48532ec9.js"
+  }
+]);
